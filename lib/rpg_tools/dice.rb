1f2b1b917d1178a4162f79a2b628721508265761
Yohan Piron <Yinfei84@gmail.com>
2014-12-18 22:39:58 +0100

    We now return any number of dices as an array

--- lib/rpg_tools/dice.rb
+++ lib/rpg_tools/dice.rb
@@ -9,9 +9,6 @@ module RpgTools
         # I mean, seriously ?
         return nil if dices_num.zero?
 
-        # We don't need an array for less than 2 dices
-        return rand(1..max_value) if dices_num < 2
-
         [].tap do |dice_set|
           dices_num.times do
             dice_set << rand(1..max_value)
