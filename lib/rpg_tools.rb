38ae924d0c383a1f4be8704f38d6e0887bb6dfcd
Yohan Piron <Yinfei84@gmail.com>
2015-01-10 01:17:50 +0100

    fixed file requirements

--- lib/rpg_tools.rb
+++ lib/rpg_tools.rb
@@ -1,3 +1,4 @@
-require './lib/rpg_tools/card'
-require './lib/rpg_tools/coin'
-require './lib/rpg_tools/dice'
+require 'rpg_tools/card'
+require 'rpg_tools/coin'
+require 'rpg_tools/dice'
+require 'rpg_tools/dice_bag'
