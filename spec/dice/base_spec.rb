551cc78a31415289ef7e9e351ed6fcbaa37e253b
Yohan Piron <yinfei84@gmail.com>
2017-07-31 22:57:39 +0200

    Add tests

--- spec/dice/base_spec.rb
+++ spec/dice/base_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 
 describe RpgTools::Dice::Base do
   let(:dice) { described_class.new(10) }
+  let(:dice_with_modifier) { described_class.new(10, '+2') }
 
   describe '#initialize' do
     it 'creates a basic dice' do
       expect { described_class.new(10) }.not_to raise_error
+    end
+
+    it 'creates a basic dice with a modifier' do
+      expect { described_class.new(10, '+2') }.not_to raise_error
     end
 
     it 'falls back on sides_count method if no sides arg is provided' do
@@ -21,10 +26,31 @@ describe RpgTools::Dice::Base do
   describe '#roll' do
     before do
       allow(dice).to receive(:roll_calculation).and_return(10)
+      allow(dice_with_modifier).to receive(:roll_calculation).and_return(10)
     end
 
-    it 'returns an integer based on a roll_calculation method' do
-      expect(dice.roll).to eq(10)
+    context 'no modifier' do
+      it 'returns an integer based on a basic roll calculation' do
+        expect(dice.roll).to eq(10)
+      end
+
+      it 'does not call the #roll_with_modifiers method' do
+        expect(dice).not_to receive(:roll_with_modifiers)
+
+        dice.roll
+      end
+    end
+
+    context 'modifier present' do
+      it 'returns an integer based on a roll calulation plus modifier applied' do
+        expect(dice_with_modifier.roll).to eq(12)
+      end
+
+      it 'calls the #roll_with_modifiers method' do
+        expect(dice_with_modifier).to receive(:roll_with_modifiers)
+
+        dice_with_modifier.roll
+      end
     end
 
     it 'updates a dice value attribute' do
